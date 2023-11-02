e411b3dba6d4b4c13047d245d41e7da25ec0c299
mikey-fernandez2 <mikey.fernandez2@gmail.com>
2023-11-02 00:25:43 -0400

    Changed to 4 bar linkage leg and changed left leg signs

--- derive_eom.m
+++ derive_eom.m
@@ -4,8 +4,8 @@ name = 'leggedWheelchair';
 %% Define the system
 % Define variables for time, generalized coordinates + derivatives, controls, and parameters 
 syms t th1 th2 th3 th4 th5 x y phi dth1 dth2 dth3 dth4 dth5 dx dy dphi ddth1 ddth2 ddth3 ddth4 ddth5 ddx ddy ddphi real
-syms m1 m2 ma mb I_1 I_2 I_5 I_A I_B l_c1 l_c2 l_cb real
-syms l1 l2 b r g real
+syms m1 m2 m3 m4 ma mb I1 I2 I3 I4 I_A I_B real
+syms l_OA l_OB l_AC l_DE b l_O_m1 l_B_m2 l_A_m3 l_C_m4 l_cb r g real
 syms tau1 tau2 tau3 tau4 Fl_x Fl_y Fr_x Fr_y Fw_x Fw_y real
 syms Ir N real
 syms mU I_U lU phiU real % constant offset to the user with mass mU
@@ -17,7 +17,8 @@ ddq = [ddth1; ddth2; ddth3; ddth4; ddth5; ddx; ddy; ddphi];  % second time deriv
 u   = [tau1 ; tau2 ; tau3 ; tau4];                    % controls
 F   = [Fl_x ; Fl_y ; Fr_x ; Fr_y ; Fw_x ; Fw_y];      % ground reaction forces
 
-p   = [lU phiU mU I_U m1 m2 ma mb I_1 I_2 I_5 I_A I_B l1 l2 b l_c1 l_c2 l_cb r Ir N g]';        % parameters
+p = [m1 m2 m3 m4 ma mb I1 I2 I3 I4 I_A I_B l_OA l_OB l_AC l_DE b l_O_m1 l_B_m2 l_A_m3 l_C_m4 l_cb r Ir N mU I_U lU phiU g]';
+% p   = [lU phiU mU I_U m1 m2 ma mb I1 I2 I_A I_B l1 l2 b l_c1 l_c2 l_cb r Ir N g]';        % parameters
 
 %% Kinematics
 % Generate Vectors and Derivativess
@@ -29,10 +30,10 @@ xhat = [1; 0; 0];
 yhat = [0; 1; 0];
 
 ebhat =  cos(th5)*ihat             + sin(th5)*jhat;             % orientation of connecting rod
-el1hat = sin(th1 + th5)*ihat       - cos(th1 + th5)*jhat;       % orientation of link 1 of left leg
-el2hat = sin(th1 + th2 + th5)*ihat - cos(th1 + th2 + th5)*jhat; % orientation of link 2 of left leg
-er1hat = sin(th3 + th5)*ihat       - cos(th3 + th5)*jhat;       % orientation of link 1 of right leg
-er2hat = sin(th3 + th4 + th5)*ihat - cos(th3 + th4 + th5)*jhat; % orientation of link 2 of right leg
+el1hat = sin(-th5 - th1)*ihat       - cos(-th5 - th1)*jhat;       % orientation of link 1/4 of left leg - see system diagram for sign convention
+el2hat = sin(-th5 - th1 - th2)*ihat - cos(-th5 - th1 - th2)*jhat; % orientation of link 2/3 of left leg
+er1hat = sin(th3 + th5)*ihat       - cos(th3 + th5)*jhat;       % orientation of link 1/4 of right leg
+er2hat = sin(th3 + th4 + th5)*ihat - cos(th3 + th4 + th5)*jhat; % orientation of link 2/3 of right leg
 eUhat =  cos(th5 + phiU)*ihat      + sin(th5 + phiU)*jhat;      % orientation of vector from wheel axis to user 
 
 ddt = @(r) jacobian(r, [q; dq])*[dq; ddq]; % a handy anonymous function for taking time derivatives
@@ -40,29 +41,52 @@ ddt = @(r) jacobian(r, [q; dq])*[dq; ddq]; % a handy anonymous function for taki
 rA = x*ihat + y*jhat; % center of wheel
 rB = rA +  b*ebhat;  % robot hip/shoulder
 rU = rA + lU*eUhat;  % CoM of user of chair
-rC = rB + l1*el1hat; % left knee
-rD = rB + l1*er1hat; % right knee
-rE = rC + l2*el2hat; % left foot
-rF = rD + l2*er2hat; % right foot
 
-r_lc1 = rB + l_c1*el1hat; % CoM of left link 1
-r_lc2 = rC + l_c2*el2hat; % CoM of left link 2
-r_rc1 = rB + l_c1*er1hat; % CoM of right link 1
-r_rc2 = rD + l_c2*er2hat; % CoM of right link 2
+rllA = rB + l_OA*el1hat;
+rllB = rB + l_OB*el1hat; % left knee
+rllC = rllA + l_AC*el2hat;
+rllD = rllB + l_AC*el2hat;
+rllE = rllD + l_DE*el1hat; % left foot
+r_lc1 = rB + l_O_m1*el1hat;
+r_lc2 = rllB + l_B_m2*el2hat;
+r_lc3 = rllA + l_A_m3*el2hat;
+r_lc4 = rllC + l_C_m4*el1hat;
+
+rrlA = rB + l_OA*er1hat;
+rrlB = rB + l_OB*er1hat; % right knee
+rrlC = rrlA + l_AC*er2hat;
+rrlD = rrlB + l_AC*er2hat;
+rrlE = rrlD + l_DE*er1hat; % right foot
+r_rc1 = rB + l_O_m1*er1hat;
+r_rc2 = rllB + l_B_m2*er2hat;
+r_rc3 = rrlA + l_A_m3*er2hat;
+r_rc4 = rrlC + l_C_m4*er1hat;
+
 r_cb = rA + l_cb*ebhat; % CoM of connecting rod
+
+drllA = ddt(rllA);
+drllB = ddt(rllB);
+drllc = ddt(rllC);
+drllD = ddt(rllD);
+drllE = ddt(rllE);
+drl_c1 = ddt(r_lc1);
+drl_c2 = ddt(r_lc2);
+drl_c3 = ddt(r_lc3);
+drl_c4 = ddt(r_lc4);
+
+drrlA = ddt(rrlA);
+drrlB = ddt(rrlB);
+drrlc = ddt(rrlC);
+drrlD = ddt(rrlD);
+drrlE = ddt(rrlE);
+drr_c1 = ddt(r_rc1);
+drr_c2 = ddt(r_rc2);
+drr_c3 = ddt(r_rc3);
+drr_c4 = ddt(r_rc4);
 
 drA = ddt(rA);
 drB = ddt(rB);
-drC = ddt(rC);
-drD = ddt(rD);
-drE = ddt(rE);
-drF = ddt(rF);
 drU = ddt(rU);
-
-dr_lc1 = ddt(r_lc1);
-dr_lc2 = ddt(r_lc2);
-dr_rc1 = ddt(r_rc1);
-dr_rc2 = ddt(r_rc2);
 dr_cb = ddt(r_cb);
 
 %% Kinetics
@@ -71,56 +95,58 @@ F2Q = @(F, r) simplify(jacobian(r, q)'*(F));    % force contributions to general
 M2Q = @(M, w) simplify(jacobian(w, dq)'*(M));   % moment contributions to generalized forces
 
 % angular velocities
-omega_l1 = dth5 + dth1;
-omega_l2 = dth5 + dth1 + dth2;
-omega_r1 = dth5 + dth3;
-omega_r2 = dth5 + dth3 + dth4;
+omega_l1 = dth5 - dth1; % left links 1/4
+omega_l2 = dth5 - dth1 - dth2; % left links 2/3
+omega_r1 = dth5 + dth3; % right links 1/4
+omega_r2 = dth5 + dth3 + dth4; % right links 2/3
 omega_U =  dth5; % constant offset, so the angular velocity matches the connecting rod tilt
 
 % kinetic energies
 T_A  = (1/2)*ma*dot(drA, drA)       + (1/2)*I_A*dphi^2;
 T_B  = (1/2)*mb*dot(dr_cb, dr_cb)   + (1/2)*I_B*dth5^2;
 T_U  = (1/2)*mU*dot(drU, drU)       + (1/2)*I_U*omega_U^2;
-T_l1 = (1/2)*m1*dot(dr_lc1, dr_lc1) + (1/2)*I_1*omega_l1^2;
-T_l2 = (1/2)*m2*dot(dr_lc2, dr_lc2) + (1/2)*I_2*omega_l2^2;
-T_r1 = (1/2)*m1*dot(dr_rc1, dr_rc1) + (1/2)*I_1*omega_r1^2;
-T_r2 = (1/2)*m2*dot(dr_rc2, dr_rc2) + (1/2)*I_2*omega_r2^2;
+T_l1 = (1/2)*m1*dot(drl_c1, drl_c1) + (1/2)*I1*omega_l1^2;
+T_l2 = (1/2)*m2*dot(drl_c2, drl_c2) + (1/2)*I2*omega_l2^2;
+T_l3 = (1/2)*m3*dot(drl_c3, drl_c3) + (1/2)*I3*omega_l2^2;
+T_l4 = (1/2)*m4*dot(drl_c4, drl_c4) + (1/2)*I4*omega_l1^2;
+T_r1 = (1/2)*m1*dot(drr_c1, drr_c1) + (1/2)*I1*omega_r1^2;
+T_r2 = (1/2)*m2*dot(drr_c2, drr_c2) + (1/2)*I2*omega_r2^2;
+T_r3 = (1/2)*m3*dot(drr_c3, drr_c4) + (1/2)*I3*omega_r2^2;
+T_r4 = (1/2)*m4*dot(drr_c4, drr_c4) + (1/2)*I4*omega_r1^2;
 
-T_l1_rotor = (1/2)*Ir*(dth5 + N*dth1)^2;
-T_l2_rotor = (1/2)*Ir*(dth5 + dth1 + N*dth2)^2;
+T_l1_rotor = (1/2)*Ir*(dth5 - N*dth1)^2;
+T_l2_rotor = (1/2)*Ir*(dth5 - dth1 - N*dth2)^2;
 T_r1_rotor = (1/2)*Ir*(dth5 + N*dth3)^2;
 T_r2_rotor = (1/2)*Ir*(dth5 + dth3 + N*dth4)^2;
 
 Vg_A  = ma*g*dot(rA, jhat);
 Vg_B  = mb*g*dot(r_cb, jhat);
 Vg_U  = mU*g*dot(rU, jhat);
-Vg_l1 = m1*g*dot(r_lc1, jhat);
-Vg_l2 = m2*g*dot(r_lc2, jhat);
-Vg_r1 = m1*g*dot(r_rc1, jhat);
-Vg_r2 = m2*g*dot(r_rc2, jhat);
+Vg_l1 = m1*g*dot(r_lc1, -jhat);
+Vg_l2 = m2*g*dot(r_lc2, -jhat);
+Vg_l3 = m3*g*dot(r_lc3, -jhat);
+Vg_l4 = m4*g*dot(r_lc4, -jhat);
+Vg_r1 = m1*g*dot(r_rc1, -jhat);
+Vg_r2 = m2*g*dot(r_rc2, -jhat);
+Vg_r3 = m3*g*dot(r_rc3, -jhat);
+Vg_r4 = m4*g*dot(r_rc4, -jhat);
 
-T = simplify(T_A + T_B + T_U + T_l1 + T_l2 + T_r1 + T_r2 + T_l1_rotor + T_l2_rotor + T_r1_rotor + T_r2_rotor);
-Vg = simplify(Vg_A + Vg_B + Vg_U + Vg_l1 + Vg_l2 + Vg_r1 + Vg_r2);
+T = simplify(T_A + T_B + T_U + T_l1 + T_l2 + T_l3 + T_l4 + T_r1 + T_r2 + T_r3 + T_r4 + T_l1_rotor + T_l2_rotor + T_r1_rotor + T_r2_rotor);
+Vg = simplify(Vg_A + Vg_B + Vg_U + Vg_l1 + Vg_l2 + Vg_l3 + Vg_l4 + Vg_r1 + Vg_r2 + Vg_r3 + Vg_r4);
 
+% motor torques
 Q_tau1 = M2Q(tau1*khat, omega_l1*khat);
 Q_tau2 = M2Q(tau2*khat, omega_l2*khat); 
 Q_tau2R = M2Q(-tau2*khat, omega_l1*khat);
 Q_tau3 = M2Q(tau3*khat, omega_r1*khat);
 Q_tau4 = M2Q(tau4*khat, omega_r2*khat); 
 Q_tau4R = M2Q(-tau4*khat, omega_r1*khat);
-% motor torques don't impact th5, that's a driven variable
-% Q_tau1 = M2Q(tau1*khat, (omega_l1 - dth5)*khat);
-% Q_tau2 = M2Q(tau2*khat, (omega_l2 - dth5)*khat); 
-% Q_tau2R = M2Q(-tau2*khat, (omega_l1 - dth5)*khat);
-% Q_tau3 = M2Q(tau3*khat, (omega_r1 - dth5)*khat);
-% Q_tau4 = M2Q(tau4*khat, (omega_r2 - dth5)*khat); 
-% Q_tau4R = M2Q(-tau4*khat, (omega_r1 - dth5)*khat);
 
 Q_tau = Q_tau1 + Q_tau2 + Q_tau2R + Q_tau3 + Q_tau4 + Q_tau4R;
 Q = Q_tau;
 
 % Assemble the array of cartesian coordinates of the key points
-keypoints = [rA(1:2) rB(1:2) rC(1:2) rD(1:2) rE(1:2) rF(1:2) rU(1:2)];
+keypoints = [rA(1:2) rB(1:2) rllA(1:2) rllB(1:2) rllC(1:2) rllD(1:2) rllE(1:2) rrlA(1:2) rrlB(1:2) rrlC(1:2) rrlD(1:2) rrlE(1:2) rU(1:2)];
 
 %% All the work is done!  Just turn the crank...
 % Derive Energy Function and Equations of Motion
@@ -139,7 +165,7 @@ Grav_Joint_Sp = simplify(jacobian(Vg, q)');
 Corr_Joint_Sp = simplify(eom + Q - Grav_Joint_Sp - A*ddq);
 
 % Compute feet jacobian
-J = jacobian([rE; rF], q);
+J = jacobian([rllE; rrlE], q);
 
 % Compute ddt( J )
 dJ = reshape(ddt(J(:)), size(J));
@@ -147,10 +173,10 @@ dJ = reshape(ddt(J(:)), size(J));
 % Write Energy Function and Equations of Motion
 z  = [q; dq];
 
-rFeet = [rE(1:2) rF(1:2)];
-drFeet = [drE(1:2) drF(1:2)];
-J  = J([1 2 4 5], :);
-dJ = dJ([1 2 4 5], :);
+rFeet = [rllE(1:2) rrlE(1:2)];
+drFeet = [drllE(1:2) drrlE(1:2)];
+J  = J(1:4, :);
+dJ = dJ(1:4, :);
 
 matlabFunction(A, 'file', ['Derivation/A_' name], 'vars', {z p});
 matlabFunction(b, 'file', ['Derivation/b_' name], 'vars', {z u p});
---
+++ simulate_leggedWheelchair.m
@@ -0,0 +1,175 @@
+function simulate_leggedWheelchair()
+    setpath  % add subfolders
+    
+    restitution_coeff = 0.1;
+    friction_coeff = 0.3;
+    ground_height = 0;
+
+    %% Define fixed paramters
+    % Lengths in meters, angles in radians, mass in kg
+    lU = 0.110; % distance from wheel center to "user" CoM
+    phiU = deg2rad(80); % angle from axis to "user" CoM
+    mU = 0.03; % 30 grams (2 oz.)
+    I_U = 1/12 * mU * lU^2; % approximate as slender rod
+    m1 = .0393 + .2; % from lab
+    m2 = .0368; % from lab
+    m3 = .00783; % from lab
+    m4 = .0155; % from lab
+    m5 = 0.0424 * 12 / 2.205; % 12" length of 80/20 1010 profile
+    ma = 0.200; % 100 grams per wheel (6 oz. each) - weigh them!
+    mb = m5 + 2.0; % total guess; mass of motors plus mounting hardware
+    b = 0.267; % 12" 80/20
+   
+    r = 0.035; % wheels from 2.007 lab
+
+    l_OA = 0.011; % same as l_OA from lab (not quite accurate)
+    l_OB = 0.042; % same as l_OB from lab (not quite accurate)
+    l_AC = 0.096; % same as l_AC from lab (not quite accurate)
+    l_DE = 0.091; % same as l_DE from lab (not quite accurate)
+    l_O_m1 = 0.032; % l_O_m1
+    l_B_m2 = 0.0344; % l_B_m2
+    l_A_m3 = 0.0622; % l_A_m3 from lab
+    l_C_m4 = 0.0610; % l_C_m4 from lab (revisit this)
+    l_cb = 0.1524; % 6" (doesn't account for amount hanging off past connection points)
+
+    I1 = 25.1 * 10^-6;
+    I2 = 53.5 * 10^-6;
+    I3 = 9.25 * 10^-6;
+    I4 = 22.176 * 10^-6;
+    I_A = 0.5 * ma * r^2; % thin solid disk
+    I_B = 1 * 10^-3; % truly a random guess. Need to do more calcs
+    
+    N = 18.75;
+    Ir = 0.0035/N^2;
+    g = 9.81;
+
+    %% Parameter vector
+    % p   = [lU phiU mU I_U m1 m2 ma mb I_1 I_2 I_5 I_A I_B l1 l2 b l_c1 ...
+    %     l_c2 l_cb r Ir N g]';
+    p = [m1 m2 m3 m4 ma mb I1 I2 I3 I4 I_A I_B l_OA l_OB l_AC l_DE b l_O_m1 l_B_m2 l_A_m3 l_C_m4 l_cb r Ir N mU I_U lU phiU g]';
+
+    %% Perform Dynamic simulation
+    dt = 0.001;
+    tf = 10;
+    num_steps = floor(tf/dt);
+    tspan = linspace(0, tf, num_steps); 
+    % order of generalized coordinates [th1  ; th2  ; th3  ; th4  ; th5  ; x  ; y  ; phi  ];
+    z0 = [pi/8; pi/4; pi/8; pi/4; pi/4; 0.5; 0.5; 0; 0; 0; 0; 0; 0; 0; 0; 0];
+    q = 1:length(z0)/2; dq = (length(z0)/2 + 1):(length(z0));
+    z_out = zeros(length(z0), num_steps);
+    z_out(:, 1) = z0;
+    
+    for i = 1:num_steps - 1
+        dz = dynamics(z_out(:, i), p);
+
+        z_out(dq, i + 1) = z_out(dq, i) + dz(dq)*dt;
+        z_out(q, i + 1) = z_out(q, i) + z_out(dq, i + 1)*dt;
+                
+    end
+
+    %% Animate Solution
+    figure(1); clf;
+    
+    % Prepare plot handles
+    hold on
+    h_AB = plot([0], [0], 'LineWidth', 2);
+    h_AU = plot([0], [0], 'LineWidth', 2);
+    h_l1 = plot([0], [0], 'LineWidth', 2);
+    h_l2 = plot([0], [0], 'LineWidth', 2);
+    h_l3 = plot([0], [0], 'LineWidth', 2);
+    h_l4 = plot([0], [0], 'LineWidth', 2);
+    h_r1 = plot([0], [0], 'LineWidth', 2);
+    h_r2 = plot([0], [0], 'LineWidth', 2);
+    h_r3 = plot([0], [0], 'LineWidth', 2);
+    h_r4 = plot([0], [0], 'LineWidth', 2);
+    xlabel('x')
+    ylabel('y');
+    h_title = title('t = 0.0s');
+    
+    axis equal
+%     axis([-0.5 0.5 -0.5 0.5]);
+    skip_frame = 100;
+    
+    % Step through and update animation
+    for i = 1:num_steps
+        if mod(i, skip_frame)
+            continue
+        end
+        % interpolate to get state at current time.
+        t = tspan(i);
+        z = z_out(:, i);
+        keypoints = keypoints_leggedWheelchair(z, p);
+        
+        % keypoints = [rA rB  rrlA  rrlB  rrlC  rrlD  rrlE  rllA  rllB  rllC  rllD  rllE  rU ];
+        % Get vectors to keypoints
+        rA = keypoints(:, 1);
+        rB = keypoints(:, 2);
+        rllA = keypoints(:, 3);
+        rllB = keypoints(:, 4);
+        rllC = keypoints(:, 5);
+        rllD = keypoints(:, 6);
+        rllE = keypoints(:, 7);
+        rrlA = keypoints(:, 8);
+        rrlB = keypoints(:, 9);
+        rrlC = keypoints(:, 10);
+        rrlD = keypoints(:, 11);
+        rrlE = keypoints(:, 12);
+        rU = keypoints(:, 13);
+
+        set(h_title, 'String', sprintf('t = %.2f', t)); % update title
+
+        % plot circle
+        wheelHead = viscircles([rA(1) rA(2); rU'], [r 0.01]);
+
+        % Plot rod
+        set(h_AB, 'XData', [rA(1) rB(1)]);
+        set(h_AB, 'YData', [rA(2) rB(2)]);
+
+        % Plot user
+        set(h_AU, 'XData', [rA(1) rU(1)]);
+        set(h_AU, 'YData', [rA(2) rU(2)]);
+
+        % Plot legs
+        set(h_l1, 'XData', [rB(1) rllB(1)]);
+        set(h_l1, 'YData', [rB(2) rllB(2)]);
+        set(h_l2, 'XData', [rllA(1) rllC(1)]);
+        set(h_l2, 'YData', [rllA(2) rllC(2)]);
+        set(h_l3, 'XData', [rllB(1) rllD(1)]);
+        set(h_l3, 'YData', [rllB(2) rllD(2)]);
+        set(h_l4, 'XData', [rllC(1) rllE(1)]);
+        set(h_l4, 'YData', [rllC(2) rllE(2)]);
+
+        set(h_r1, 'XData', [rB(1) rrlB(1)]);
+        set(h_r1, 'YData', [rB(2) rrlB(2)]);
+        set(h_r2, 'XData', [rrlA(1) rrlC(1)]);
+        set(h_r2, 'YData', [rrlA(2) rrlC(2)]);
+        set(h_r3, 'XData', [rrlB(1) rrlD(1)]);
+        set(h_r3, 'YData', [rrlB(2) rrlD(2)]);
+        set(h_r4, 'XData', [rrlC(1) rrlE(1)]);
+        set(h_r4, 'YData', [rrlC(2) rrlE(2)]);
+
+        if i == skip_frame
+            waitforbuttonpress % dont simulate until you start
+        end
+        pause(.01)
+        delete(wheelHead)
+    end
+wheelHead = viscircles([rA(1) rA(2); rU'], [r 0.01]);
+end
+
+function dz = dynamics(z,p)
+    % Get mass matrix
+    A = A_leggedWheelchair(z,p);
+    
+    % Get forces
+    u = [0 0 0 0]';
+    b = b_leggedWheelchair(z, u, p);
+    
+    % Solve for qdd
+    qdd = A\b;
+    
+    % Form dz
+    dz = 0*z;
+    dz(1:8) = z(9:16);
+    dz(9:16) = qdd;
+end
